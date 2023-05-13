8bc2fa5361fc5f4ab992272b3bc7ca0d8d416e5f
Ikeda-Luiz <ikeda.luiz00@gmail.com>
2023-05-13 00:31:41 +0000

    arquivo gasolina.csv, gasolina.py e gasolina.png alterados

--- gasolina.py
+++ gasolina.py
@@ -4,14 +4,13 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # - coleta de dados;
-
-gasoline_df = pd.read_csv("gasolina.csv", sep=",")
-
+gasoline_df = pd.read_csv('gasolina.csv', sep=',')
 # - grafico;
-grafico = sns.lineplot(data=gasoline_df, x="dia", y="venda")
-grafico.set_title("Preço da Gasolina versus Dia")
+grafico = sns.lineplot(data=gasoline_df, x='dia', y='venda')
+grafico.set_title('Preço da Gasolina versus Dia')
 grafico.set_xlabel('Dia')
 grafico.set_ylabel('Preço (BRL)')
-
 # - salvar o grafico;
 plt.savefig('gasolina.png')
+# mensagem de finalização
+print('sucesso')
